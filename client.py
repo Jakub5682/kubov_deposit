a5cf1958f66b8a7bcfac0eb885a3750b905f2a84
Jakub <skjakubovci9040@gmail.com>
2020-02-13 10:54:09 +0100

    socket123

---
+++ client.py
@@ -0,0 +1,16 @@
+import socket
+
+sock = socket.socket(socket.AF_INET , socket.SOCK_DGRAM)
+
+server_addr = ('192.168.5.145', 12345)
+
+sock.sendto('hello, jakub was there(and marko too)'.encode('utf-8'), server_addr)
+
+
+
+
+
+
+
+
+
